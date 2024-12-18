5c4e872ef6b26d278eadfa45dc19faf1c806e430
Roshan Kumar Singh <156603960+RKS1999@users.noreply.github.com>
2024-12-18 16:10:17 +0530

    all done

--- config/educations.tsx
+++ config/educations.tsx
@@ -11,9 +11,9 @@ export const Educations: {
   [key in Company]: CompanyDetail
 } = {
   BSEBP: {
-    name: "BIHAR SCHOOL EXAMINATION BOARD, PATNA",
-    longName: "BIHAR SCHOOL EXAMINATION BOARD, PATNA",
-    duration: "Matric Pass at 1998",
+    name: "BIHAR SCHOOL, BIHAR",
+    longName: "BIHAR SCHOOL, BIHAR",
+    duration: "8th Pass at 1996",
     roles: [<>I completed my 8th, Bihar in 1996.</>],
   },
 }
